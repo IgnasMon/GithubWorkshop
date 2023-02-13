f75980cf639320a8bd55e3ab8f82b0061fa9f9c0
Novasky-Guy <joas@vanboxtel.org>
2023-02-13 11:21:24 +0100

    Calculator

--- WeLearntoUseGithubTheGayWay/WeLearntoUseGithubTheGayWay/Form1.cs
+++ WeLearntoUseGithubTheGayWay/WeLearntoUseGithubTheGayWay/Form1.cs
@@ -6,5 +6,15 @@ namespace WeLearntoUseGithubTheGayWay
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
